0ad04ae60ddbcd81e89a84d76d356737f6074763
Varun Krishna. P <Varunkrishna.dbz@gmail.com>
2015-09-12 22:47:58 +0530

    Update add.java

--- Core Java/Lecture 01/add.java
+++ Core Java/Lecture 01/add.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 /****************************************************************************************
-when the class is declared as public, then 						*
-the class name and the file name should be the 						*
-same. if the class is not declared public, you 						*
-can compile the program with javac filename.java,					*
-you should run the program with the following command					*
-java ClassName instead of FileName							*
-example											*
--------											*
-F:\Lang\Java\NIIT CJEA06\Core Java\Lecture 01>javac add.java				*
-add.java:4: error: class Add is public, should be declared in a file named Add.java	*
-public class Add{									*
-       ^										*
-1 error											*
-											*
-F:\Lang\Java\NIIT CJEA06\Core Java\Lecture 01>javac add.java				*
-											*
-F:\Lang\Java\NIIT CJEA06\Core Java\Lecture 01>java Add					*
-Enter first number: 123									*
-Enter second number: 123								*
-The sum of two numbers is: 246								*
+*when the class is declared as public, then 						*
+*the class name and the file name should be the 					*
+*same. if the class is not declared public, you 					*
+*can compile the program with javac filename.java,					*
+*you should run the program with the following command					*
+*java ClassName instead of FileName							*
+*example										*
+*-------										*
+*F:\Lang\Java\NIIT CJEA06\Core Java\Lecture 01>javac add.java				*
+*add.java:4: error: class Add is public, should be declared in a file named Add.java	*
+*public class Add{									*
+*      ^										*
+*1 error										*
+*											*
+*F:\Lang\Java\NIIT CJEA06\Core Java\Lecture 01>javac add.java				*
+*											*
+*F:\Lang\Java\NIIT CJEA06\Core Java\Lecture 01>java Add					*
+*Enter first number: 123								*
+*Enter second number: 123								*
+*The sum of two numbers is: 246								*
 ****************************************************************************************/
 class Add{
 	public static void main(String args[]){
