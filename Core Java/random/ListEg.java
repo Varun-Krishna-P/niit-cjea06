1d0140a19573274e0e59f071d7e9d4851ca6cc00
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2015-10-25 11:26:15 +0530

    1. Array List example.

---
+++ Core Java/random/ListEg.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class ListEg{
+	public static void main(String args[]){
+		List list=new ArrayList();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add("Hello");
+		list.add("World");
+		System.out.println(list);
+	}
+}
